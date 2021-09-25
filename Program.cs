f0db3ebd56c0069188bf0d945726fb595721bc1b
Satoru Fujimori <satoru.fujimori@gmail.com>
2021-09-25 13:11:47 +0900

    Create a pdf file of  multi pages

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace iTextSharpSample
 {
@@ -6,7 +9,39 @@ namespace iTextSharpSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                using var fs = new FileStream(@".\sample.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+                var document = new Document(PageSize.A4, 25, 25, 30, 30);
+                var writer = PdfWriter.GetInstance(document, fs);
+
+                // Add meta information to the document
+                document.AddAuthor("Micke Blomquist");
+                document.AddCreator("Sample application using iTestSharp");
+                document.AddKeywords("PDF tutorial education");
+                document.AddSubject("Document subject - Describing the steps creating a PDF document");
+                document.AddTitle("The document title - PDF creation using iTextSharp");
+
+                // Open the document to enable you to write to the document
+                document.Open();
+
+                // Add a simple and wellknown phrase to the document
+                for (int x = 0; x != 100; x++)
+                {
+                    document.Add(new Paragraph("Paragraph - Hello World!"));
+                }
+
+                // Close the document
+                document.Close();
+                writer.Close();
+                fs.Close();
+
+                Console.WriteLine("Document saved to the pdf folder.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"error msg={ex.Message}");
+            }
         }
     }
 }
